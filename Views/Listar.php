f8478ee20cbd2b035d8653d5c8eeaa4a26d73fdb
Sala D3 <CSalaD3@uniboyaca.edu.co>
2025-06-07 09:44:44 -0500

    6

--- Views/Listar.php
+++ Views/Listar.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Style.css">
+
     <title>Listar</title>
 </head>
 <body>
+    <a href="index.php">Volver</a>
     
 </body>
 </html>
--- Views/index.php
+++ Views/index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Style.css">
     <title>inicio</title>
 </head>
 <body>
     <a href="Registrar.php">Registrar Inventario</a>
+    <a href="Listar.php">Listar Inventario</a>
+
 </body>
 </html>

85245a3a34144ffd0f693b3d660925c6be9b6655
Sala D3 <CSalaD3@uniboyaca.edu.co>
2025-06-07 08:47:33 -0500

    1

---
+++ Views/Registrar.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar</title>
+</head>
+<body>
+    <a href="index.php">Volver</a>
+</body>
+</html>
